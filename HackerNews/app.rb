09dd1778cf6b18777a8824bac26c718595b064d7
JackDrogon <jack.xsuperman@gmail.com>
2015-05-20 00:34:58 +0800

    Add TODO && add some interface

--- HackerNews/app.rb
+++ HackerNews/app.rb
@@ -4,5 +4,42 @@
 require 'sinatra'
 
 get '/' do
-  'Welcome to HackerNews'
+  content_type :txt
+  'Welcome to HackerNews.'
+end
+
+## Blog
+# @doc: list article
+# redis list
+get '/:year/:month/:day' do
+  "#{params['year']}-#{params['month']}-#{params['day']}'s Articles."
+end
+
+# Use redis hash, mysql (with datetime author...)
+get '/aritles/:name' do
+  "You get artile #{params['name']}!"
+end
+
+get '/cateory' do
+end
+
+get '/rss' do
+end
+
+get '/login' do
+end
+
+post '/login' do
+end
+
+get '/logout' do
+end
+
+get '/api' do
+end
+
+get '/api/login' do
+end
+
+get '/api/logout' do
 end
